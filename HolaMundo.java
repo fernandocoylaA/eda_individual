4cb55a8580dc9684bf2166bf7a12a1ba63946af6
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-05-07 19:37:52 -0500

    Bienvenida al curso

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,10 @@ public class HolaMundo{
 	public static void main(String args[]){
 		/*Impresion de un Hola Mundo*/
 		System.out.println("¡Hola Mundo, Saludos desde Laboratorio EDA!");
+		
+		/*Bienvenida al Curso*/
+		System.out.println("¡Bienvenidos al  Laboratorio de Estructura de Datos y Algoritmos!");
 
+		
 	}
 }
